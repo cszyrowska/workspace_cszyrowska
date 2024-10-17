3cc1b3b75e0c553dac26bf6129ddf6e3a6c23d67
cszyrowska <cecyliaszyrowska@gmail.com>
2024-10-17 11:42:31 +0100

    added url to github website

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/workspace_cszyrowska/', // Replace 'your-repository-name' with your actual GitHub repository name
   plugins: [react()],
+  build: {
+    outDir: 'dist'  // Ensures the build outputs to the 'dist' directory
+  }
 })
